0d42813a852dd91532b1800482bbda8862485c63
Chen Wilson <chenlwilson@gmail.com>
2019-02-24 07:58:41 -0800

    Fixed showtime service's url

--- proxy-server/app.js
+++ proxy-server/app.js
@@ -1,21 +1,7 @@
 var express = require('express');
 const path = require('path');
 var app = express();
-// sample path: 'some-host/m/black_panther'
-// var pathHandler = (req, res) => {
-//   const title = req.path.split('/m/')[1];
-//   if (title || title === '') {
-//     res.sendFile(path.join(__dirname, '../dist', 'index.html'))
-//   } else {
-//     res.send('can not find movie').end();
-//   }
-// }
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
 app.use('/m/:title_url', express.static(path.join(__dirname, '../dist')));
 
 app.all('/showtime/*', (req, res) => { res.redirect(`http://localhost:9002${req.path}`)});
