eb02e528a15349304e915afafcdf265f48ac4515
Chen Wilson <chenlwilson@gmail.com>
2019-03-08 19:59:37 -0800

    Changed url to https

--- proxy-server/app.js
+++ proxy-server/app.js
@@ -4,6 +4,4 @@ var app = express();
 
 app.use('/m/:title_url', express.static(path.join(__dirname, '../dist')));
 
-app.all('/showtime/*', (req, res) => { res.redirect(`http://localhost:9002${req.path}`)});
-
 module.exports = app;
